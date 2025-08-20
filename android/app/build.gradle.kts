8b87c83d0c1e4414866c1a26357946b9d6806a90
lee-jaemmin <dlwoals8546@gmail.com>
2025-08-20 13:57:16 +0900

    네이버맵에 맞게 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,39 +1,37 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.roommate"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
+    // 이미 안내한 대로 SDK/NDK도 명시 권장
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.roommate"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+
+        // ★ 여기 3줄 중요
+        minSdk = 23          // ← 21에서 23으로 올림
+        targetSdk = 36       // ← 숫자로 명시
+        // versionCode / versionName은 flutter 변수를 그대로 써도 됨
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
