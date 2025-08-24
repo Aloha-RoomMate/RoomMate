3aefe9fdfc9bb40039bc854ee4f6d15404356289
lee-jaemmin <dlwoals8546@gmail.com>
2025-08-24 22:29:02 +0900

    게시 컨테이너

--- lib/features/navigationbar/widgets/post_container.dart
+++ lib/features/navigationbar/widgets/post_container.dart
@@ -1,31 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:roommate/constants/gaps.dart';
 import 'package:roommate/constants/sizes.dart';
+import 'package:roommate/features/view/room_owner_post_view.dart';
 
 class PostContainer extends StatelessWidget {
   const PostContainer({super.key});
 
+  void _onContainerTap(BuildContext context) {
+    Navigator.of(
+      context,
+    ).push(
+      MaterialPageRoute(
+        builder: (context) => RoomOwnerPostView(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 0.85,
-      heightFactor: 0.2,
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: Sizes.size12,
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Column(
-              children: [
-                Text('제목'),
-                Text('위치'),
-              ],
-            ),
-          ],
-        ),
-        decoration: BoxDecoration(
-          color: Colors.red,
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: Sizes.size12,
+        vertical: Sizes.size4,
+      ),
+      child: GestureDetector(
+        onTap: () => _onContainerTap(context),
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.98,
+          height: 100,
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size8,
+            vertical: Sizes.size8,
+          ),
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor.withValues(alpha: 0.1),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    width: 84,
+                    height: 84,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Sizes.size12),
+                      image: DecorationImage(
+                        image: AssetImage('assets/house.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Gaps.h24,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '제목입니다.',
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.locationDot,
+                        size: Sizes.size12,
+                      ),
+                      Gaps.h4,
+                      Text('서울시 강북구 송중동'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.calendar,
+                        size: Sizes.size12,
+                      ),
+                      Gaps.h4,
+                      Text('2025-09-10'),
+                    ],
+                  ),
+                ],
+              ),
+              Gaps.h52,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.arrowRight,
+                    size: Sizes.size16,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
