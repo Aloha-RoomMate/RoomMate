8a1f9bb7afda1979b292d12bd12da7794f0df804
ozymandiza421 <skyijin2@gmail.com>
2025-09-01 11:53:22 +0900

    /// 로 학교ip, 집 ip 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
-import 'dart:io';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:roommate/constants/sizes.dart';
 import 'package:roommate/features/authentication/login/login_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+// (중요) $ flutter run -d RFCN100T0SX --dart-define=EMU_HOST=172.30.1.99 << 이런 형식으로 기기이름이랑, 현재 IPv4 를 적으면 연결 가능하다
+// 10.0.2.2 << 이건 안드로이드 에뮬레이터 전용이다. 이걸 먼저 탐색하기 때문에 이 IP를 모르는 공기계는 어리둥절 하게 된다. 때문에 위와같은 방법으로 강제하면 된다.
+// flutter run -d RFCN100T0SX --dart-define=EMU_HOST=172.30.1.99 우진현 집
+// flutter run -d RFCN100T0SX --dart-define=EMU_HOST=10.28.32.230 학교
 
 const _AUTH_PORT = 9099;
 const _FS_PORT = 8080;
 
 const _EMU_HOST_FROM_ENV = String.fromEnvironment('EMU_HOST', defaultValue: '');
 
-// (중요) $ flutter run -d RFCN100T0SX --dart-define=EMU_HOST=172.30.1.99 << 이런 형식으로 기기이름이랑, 현재 IPv4 를 적으면 연결 가능하다 !!
-// 10.0.2.2 << 이건 안드로이드 에뮬레이터 전용이다. 이걸 먼저 탐색하기 때문에 이 IP를 모르는 공기계는 어리둥절 하게 된다. 때문에 위와같은 방법으로 강제하면 된다.
 Future<String> _pickEmuHost() async {
   if (_EMU_HOST_FROM_ENV.isNotEmpty) {
     return _EMU_HOST_FROM_ENV;
@@ -24,17 +27,23 @@ Future<String> _pickEmuHost() async {
   return '127.0.0.1';
 }
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp();
 
   if (kDebugMode) {
     final host = await _pickEmuHost();
-    await FirebaseAuth.instance.useAuthEmulator(host, _AUTH_PORT);
-    FirebaseFirestore.instance.useFirestoreEmulator(host, _FS_PORT);
-    debugPrint('🔌 Emulators -> $host:$_AUTH_PORT / $host:$_FS_PORT');
-  }
+    await FirebaseAuth.instance.useAuthEmulator(host, 9099);
+    FirebaseFirestore.instance.useFirestoreEmulator(host, 8080);
 
+    FirebaseFirestore.instance.settings = const Settings(
+      sslEnabled: false,
+      persistenceEnabled: false,
+    );
+
+    debugPrint('🔌 Emulators -> $host:9099 / $host:8080');
+  }
   await FlutterNaverMap().init(
     clientId: '7j2w13vo27',
     onAuthFailed: (e) => debugPrint('NaverMap auth fail: $e'),
@@ -45,6 +54,8 @@ Future<void> main() async {
 
 class RoomMate extends StatelessWidget {
   const RoomMate({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -63,7 +74,7 @@ class RoomMate extends StatelessWidget {
           ),
         ),
       ),
-      home: const LoginScreen(),
+      home: LoginScreen(),
     );
   }
 }
