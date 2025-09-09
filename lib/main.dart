e6fe0638287c0dd2899db93aa90e88f97887f021
ozymandiza421 <skyijin2@gmail.com>
2025-09-09 19:57:20 +0900

    [DEL] 불필요 import 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:roommate/constants/sizes.dart';
-import 'package:roommate/features/authentication/login/login_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:roommate/features/authentication/login/login_screen.dart';
 
 // (중요) $ flutter run -d RFCN100T0SX --dart-define=EMU_HOST=172.30.1.99 << 이런 형식으로 기기이름이랑, 현재 IPv4 를 적으면 연결 가능하다
 // 10.0.2.2 << 이건 안드로이드 에뮬레이터 전용이다. 이걸 먼저 탐색하기 때문에 이 IP를 모르는 공기계는 어리둥절 하게 된다. 때문에 위와같은 방법으로 강제하면 된다.
 // flutter run -d RFCN100T0SX --dart-define=EMU_HOST=172.30.1.99 우진현 집
-// flutter run -d RFCN100T0SX --dart-define=EMU_HOST=10.28.32.230 학교
+// flutter run -d RFCN100T0SX --dart-define=EMU_HOST=10.28.30.242 학교
 
 const _AUTH_PORT = 9099;
 const _FS_PORT = 8080;
