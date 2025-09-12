eba60e208c02a1771be9efdd62733d75a279cd7e
ozymandiza421 <skyijin2@gmail.com>
2025-09-12 20:44:16 +0900

    [FEAT] firebase 에서 데이터 가져오기 구현

--- lib/features/navigationbar/screens/mypage_screen.dart
+++ lib/features/navigationbar/screens/mypage_screen.dart
@@ -1,5 +1,9 @@
 // 샤라웃 투 https://github.com/youngsoonoh/youtube_profile/tree/example1 오용순
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:roommate/class/app_user.dart';
+import 'package:roommate/class/user_repository.dart';
 import 'package:roommate/constants/sizes.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
@@ -15,6 +19,10 @@ class MypageScreen extends StatefulWidget {
 
 class _MypageScreenState extends State<MypageScreen> {
   File? _profileImage;
+
+  Future<AppUser?> _getMyData() async {
+    return await UserRepository().fetchMe();
+  }
 
   Future<void> _getPhotoLibraryImage() async {
     final picked = await ImagePicker().pickImage(
@@ -91,113 +99,168 @@ class _MypageScreenState extends State<MypageScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(Sizes.size12),
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          const SizedBox(height: 12),
-          Center(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 60,
-                  backgroundColor: Colors.grey.shade200,
-                  backgroundImage: _profileImage != null
-                      ? FileImage(_profileImage!)
-                      : null,
-                  child: _profileImage == null
-                      ? Icon(
-                          Icons.person,
-                          size: 48,
-                          color: Colors.grey.shade600,
-                        )
-                      : null,
-                ),
-                const SizedBox(height: 2),
-                TextButton.icon(
-                  onPressed: () => _showBottomSheet(),
-                  icon: const Icon(Icons.photo_library_outlined),
-                  label: const Text('프로필 사진 변경'),
-                  style: TextButton.styleFrom(
-                    foregroundColor: Theme.of(context).primaryColor,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 100, left: 100),
-                  child: Column(
-                    children: [
-                      Text(
-                        "여기 닉네임이 들어가야함",
-                        style: TextStyle(
-                          fontSize: Sizes.size16,
-                          fontWeight: FontWeight.w800,
-                        ),
+    return FutureBuilder<AppUser?>(
+      future: _getMyData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (!snapshot.hasData || snapshot.data == null) {
+          return Center(
+            child: Text("데이터가 없습니다."),
+          );
+        }
+        final data = snapshot.data!;
+
+        return Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const SizedBox(height: 12),
+              Center(
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundColor: Colors.grey.shade200,
+                      backgroundImage: _profileImage != null
+                          ? FileImage(_profileImage!)
+                          : null,
+                      child: _profileImage == null
+                          ? Icon(
+                              Icons.person,
+                              size: 48,
+                              color: Colors.grey.shade600,
+                            )
+                          : null,
+                    ),
+                    const SizedBox(height: 2),
+                    TextButton.icon(
+                      onPressed: () => _showBottomSheet(),
+                      icon: const Icon(Icons.photo_library_outlined),
+                      label: const Text('프로필 사진 변경'),
+                      style: TextButton.styleFrom(
+                        foregroundColor: Theme.of(context).primaryColor,
                       ),
-                      Divider(
-                        height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 100, left: 100),
+                      child: Column(
+                        children: [
+                          Text(
+                            "여기 닉네임이 들어가야함",
+                            style: TextStyle(
+                              fontSize: Sizes.size16,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          Divider(
+                            height: 10,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 2),
+              SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(data.displayName),
+                ),
+              ),
+              Divider(
+                height: 1,
+                color: Theme.of(context).primaryColor.withAlpha(100),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AccordionWidget(
+                      title: " 내 생활패턴",
+                      content: Wrap(
+                        spacing: 6,
+                        runSpacing: 6,
+                        children: [
+                          for (final day in data.dailyRhythm?.workDays ?? [])
+                            ChipButton(text: "출근일 $day", isSelected: true),
+                          if (data.dailyRhythm?.isJobLess == true) ...[
+                            ChipButton(
+                              text:
+                                  "주말 기상시간 :${data.dailyRhythm?.weekendAwakeMins}",
+                              isSelected: true,
+                            ),
+                            ChipButton(
+                              text:
+                                  "주말 취침시간 : ${data.dailyRhythm?.weekendSleepMins}",
+                              isSelected: true,
+                            ),
+                          ],
+                        ],
+                      ),
+                    ),
+                    AccordionWidget(
+                      title: " 내 출퇴근 패턴",
+                      content: Wrap(
+                        spacing: 6,
+                        runSpacing: 6,
+                        children: [
+                          for (final day in data.dailyRhythm?.workDays ?? [])
+                            ChipButton(text: "출근일 $day", isSelected: true),
+                          if (data.dailyRhythm?.isJobLess == true) ...[
+                            ChipButton(
+                              text:
+                                  "주말 기상시간 :${data.dailyRhythm?.weekendAwakeMins}",
+                              isSelected: true,
+                            ),
+                            ChipButton(
+                              text:
+                                  "주말 취침시간 : ${data.dailyRhythm?.weekendSleepMins}",
+                              isSelected: true,
+                            ),
+                          ],
+                        ],
+                      ),
+                    ),
+                    AccordionWidget(
+                      title: " 내 식사습관",
+                      content: ChipButton(text: "asdf", isSelected: true),
+                    ),
+                    AccordionWidget(
+                      title: " 내 질병",
+                      content: Wrap(
+                        spacing: 8,
+                        children: [
+                          ChipButton(text: "asdf", isSelected: true),
+                          ChipButton(text: "text", isSelected: false),
+                        ],
+                      ),
+                    ),
+                    AccordionWidget(
+                      title: " 내 청소습관",
+                      content: ChipButton(text: "asdf", isSelected: true),
+                    ),
+                    AccordionWidget(
+                      title: " 내 잠버릇",
+                      content: ChipButton(text: "asdf", isSelected: true),
+                    ),
+                    AccordionWidget(
+                      title: " 그 이외",
+                      content: ChipButton(text: "asdf", isSelected: true),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 2),
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: const Text("달러사인 내 카테고리"),
-            ),
-          ),
-          Divider(
-            height: 1,
-            color: Theme.of(context).primaryColor.withAlpha(100),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                AccordionWidget(
-                  title: " 내 생활패턴",
-                  content: ChipButton(text: "asdf", isSelected: true),
-                ),
-                AccordionWidget(
-                  title: " 내 출/퇴근 패턴",
-                  content: ChipButton(text: "asdf", isSelected: true),
-                ),
-                AccordionWidget(
-                  title: " 내 식사습관",
-                  content: ChipButton(text: "asdf", isSelected: true),
-                ),
-                AccordionWidget(
-                  title: " 내 질병",
-                  content: Wrap(
-                    spacing: 8,
-                    children: [
-                      ChipButton(text: "asdf", isSelected: true),
-                      ChipButton(text: "text", isSelected: false),
-                    ],
-                  ),
-                ),
-                AccordionWidget(
-                  title: " 내 청소습관",
-                  content: ChipButton(text: "asdf", isSelected: true),
-                ),
-                AccordionWidget(
-                  title: " 내 잠버릇",
-                  content: ChipButton(text: "asdf", isSelected: true),
-                ),
-                AccordionWidget(
-                  title: " 그 이외",
-                  content: ChipButton(text: "asdf", isSelected: true),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
