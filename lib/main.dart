e16c52c1a40deec3b49dad4ca8f6b26432b5f553
ozymandiza421 <skyijin2@gmail.com>
2025-09-13 22:28:01 +0900

    home 바꿀때마다 이거 커밋해야하는거 화나네

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:roommate/features/authentication/login/login_screen.dart';
+import 'package:roommate/features/authentication/userinfo/roomowner_screen.dart';
+import 'package:roommate/features/authentication/userinfo/searcher_screen.dart';
+import 'package:roommate/features/authentication/userinfo/userjob_screen.dart';
+import 'package:roommate/features/category/hobby_screen.dart';
 
 // (중요) $ flutter run -d RFCN100T0SX --dart-define=EMU_HOST=172.30.1.99 << 이런 형식으로 기기이름이랑, 현재 IPv4 를 적으면 연결 가능하다
 // 10.0.2.2 << 이건 안드로이드 에뮬레이터 전용이다. 이걸 먼저 탐색하기 때문에 이 IP를 모르는 공기계는 어리둥절 하게 된다. 때문에 위와같은 방법으로 강제하면 된다.
@@ -75,7 +79,7 @@ class RoomMate extends StatelessWidget {
           ),
         ),
       ),
-      home: LoginScreen(
+      home: HobbyScreen(
         // userType: "",
         // jobKinds: "",
       ),
