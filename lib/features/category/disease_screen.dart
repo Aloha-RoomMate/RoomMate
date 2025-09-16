be9faed3bca28f151b41da09a1a76ccd95fe5763
lee-jaemmin <dlwoals8546@gmail.com>
2025-09-16 18:00:05 +0900

    [CHORE] 안 쓰는 lib 삭제

--- lib/features/category/disease_screen.dart
+++ lib/features/category/disease_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:roommate/class/app_user.dart';
 import 'package:roommate/class/user_repository.dart';
