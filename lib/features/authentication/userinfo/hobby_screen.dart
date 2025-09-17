fb269484fd84a9f96cd97412088064940d729828
lee-jaemmin <dlwoals8546@gmail.com>
2025-09-17 19:11:24 +0900

    [CHORE] 라이브러리 추가

--- lib/features/authentication/userinfo/hobby_screen.dart
+++ lib/features/authentication/userinfo/hobby_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:roommate/class/app_user.dart';
 import 'package:roommate/class/user_repository.dart';
 import 'package:roommate/constants/sizes.dart';
