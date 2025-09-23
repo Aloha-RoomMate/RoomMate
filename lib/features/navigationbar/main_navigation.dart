533358c938a18f3eae94608167d7ea91625deee0
lee-jaemmin <dlwoals8546@gmail.com>
2025-09-23 19:34:27 +0900

    [MOD] onposttap 함수 추가

--- lib/features/navigationbar/main_navigation.dart
+++ lib/features/navigationbar/main_navigation.dart
@@ -8,7 +8,8 @@ import 'package:roommate/features/navigationbar/screens/chat_screen.dart';
 import 'package:roommate/features/navigationbar/screens/home_screen.dart';
 import 'package:roommate/features/navigationbar/screens/map_screen.dart';
 import 'package:roommate/features/navigationbar/screens/mypage_screen.dart';
-import 'package:roommate/features/navigationbar/screens/mypage_screen.dart';
+import 'package:roommate/features/post/room_owner_post_screen.dart';
+import 'package:roommate/features/post/searcher_post_screen.dart';
 
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
@@ -18,7 +19,7 @@ class MainNavigation extends StatefulWidget {
 }
 
 class _MainNavigationState extends State<MainNavigation> {
-  // 현재 사용자 저장할 변수
+  // 현재 사용자 저장할 변수s
   AppUser? _currentUser;
   final UserRepository _userRepository = UserRepository();
   // 실시간 사용자 정보 저장을 위한 변수
@@ -45,12 +46,37 @@ class _MainNavigationState extends State<MainNavigation> {
     });
   }
 
+  @override
+  void dispose() {
+    // 화면이 종료될 때, 메모리 누수를 막기 위해 감시를 중단
+    _userSubscription.cancel();
+    super.dispose();
+  }
+
   void _onNavTab(int index) {
     _selectedIndex = index;
     setState(() {});
   }
 
-  void _onPostTap(UserType? userType) {}
+  void _onPostTap(AppUser? user) {
+    // 사용자가 아직 로딩 중이거나 로그아웃 상태이면 아무것도 하지 않음
+    if (user == null || user.userType == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('사용자 정보를 불러오는 중입니다...')),
+      );
+      return;
+    }
+
+    if (user.userType!.type == 'roomOwner') {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => const RoomOwnerPostScreen()),
+      );
+    } else {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => const SearcherPost()),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,7 +87,12 @@ class _MainNavigationState extends State<MainNavigation> {
         actionsPadding: EdgeInsets.only(
           right: Sizes.size20,
         ),
-        actions: [GestureDetector(child: FaIcon(FontAwesomeIcons.plus))],
+        actions: [
+          GestureDetector(
+            onTap: () => _onPostTap(_currentUser),
+            child: FaIcon(FontAwesomeIcons.plus),
+          ),
+        ],
       ),
       body: IndexedStack(
         index: _selectedIndex,
