ae7be27d3947cf26c509944a3941e5431e449e70
lee-jaemmin <dlwoals8546@gmail.com>
2025-09-29 21:05:53 +0900

    [REFACTOR] 하드코딩 > firebase에서 가져오는 것으로 변경

--- lib/features/navigationbar/widgets/post_container.dart
+++ lib/features/navigationbar/widgets/post_container.dart
@@ -1,18 +1,24 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:roommate/class/room_owner_post.dart';
 import 'package:roommate/constants/gaps.dart';
 import 'package:roommate/constants/sizes.dart';
 import 'package:roommate/features/view/room_owner_post_view.dart';
 
 class PostContainer extends StatelessWidget {
-  const PostContainer({super.key});
+  final RoomOwnerPost post;
+
+  const PostContainer({
+    super.key,
+    required this.post,
+  });
 
   void _onContainerTap(BuildContext context) {
-    Navigator.of(
-      context,
-    ).push(
+    Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => RoomOwnerPostView(),
+        builder: (context) => RoomOwnerPostView(post: post),
       ),
     );
   }
@@ -20,7 +26,7 @@ class PostContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(
+      padding: const EdgeInsets.symmetric(
         horizontal: Sizes.size12,
         vertical: Sizes.size4,
       ),
@@ -29,77 +35,74 @@ class PostContainer extends StatelessWidget {
         child: Container(
           width: MediaQuery.of(context).size.width * 0.98,
           height: 100,
-          padding: EdgeInsets.symmetric(
-            horizontal: Sizes.size8,
-            vertical: Sizes.size8,
-          ),
+          padding: const EdgeInsets.all(Sizes.size8),
           decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor.withValues(alpha: 0.1),
+            color: Colors.white,
             borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: Colors.grey.shade300),
           ),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Column(
-                children: [
-                  Container(
-                    width: 84,
-                    height: 84,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(Sizes.size12),
-                      image: DecorationImage(
-                        image: AssetImage('assets/house.jpg'),
-                        fit: BoxFit.cover,
+              // ✅ 수정: Image.network 대신 정적 Asset 이미지를 표시합니다.
+              Container(
+                width: 84,
+                height: 84,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Sizes.size12),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/house.jpg'), // TODO: 기본 이미지 경로 확인
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Gaps.h16,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      post.title ?? '제목 없음', // ✅ Null safety
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: Sizes.size18,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Gaps.h24,
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '제목입니다.',
-                    style: TextStyle(
-                      fontSize: Sizes.size20,
-                      fontWeight: FontWeight.bold,
+                    Row(
+                      children: [
+                        const FaIcon(
+                          FontAwesomeIcons.locationDot,
+                          size: Sizes.size12,
+                        ),
+                        Gaps.h4,
+                        // TODO: GeoPoint를 실제 주소 '동'으로 변환하는 로직 필요
+                        Text(post.addr != null ? '위치 정보 있음' : '위치 정보 없음'),
+                      ],
                     ),
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.locationDot,
-                        size: Sizes.size12,
-                      ),
-                      Gaps.h4,
-                      Text('서울시 강북구 송중동'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.calendar,
-                        size: Sizes.size12,
-                      ),
-                      Gaps.h4,
-                      Text('2025-09-10'),
-                    ],
-                  ),
-                ],
+                    Row(
+                      children: [
+                        const FaIcon(
+                          FontAwesomeIcons.calendar,
+                          size: Sizes.size12,
+                        ),
+                        Gaps.h4,
+                        // ✅ Null safety
+                        Text(
+                          post.movingDate != null
+                              ? DateFormat(
+                                  'yyyy-MM-dd',
+                                ).format(post.movingDate!.toDate())
+                              : '날짜 미정',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              Gaps.h52,
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FaIcon(
-                    FontAwesomeIcons.arrowRight,
-                    size: Sizes.size16,
-                  ),
-                ],
-              ),
+              const FaIcon(FontAwesomeIcons.arrowRight, size: Sizes.size16),
             ],
           ),
         ),
