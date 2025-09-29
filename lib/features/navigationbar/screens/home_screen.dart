a025a7276412d39503aa59f4d1f1ab1e5fb3bf2d
lee-jaemmin <dlwoals8546@gmail.com>
2025-09-29 21:02:35 +0900

    [REFACTOR] firebase에서 가져오는 것으로 변경

--- lib/features/navigationbar/screens/home_screen.dart
+++ lib/features/navigationbar/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:roommate/constants/sizes.dart';
-import 'package:roommate/features/navigationbar/widgets/post_container.dart';
+import 'package:roommate/features/navigationbar/widgets/post_list_view.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,9 +9,7 @@ class HomeScreen extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Column(
-        // TabBar와 TabBarView를 하나의 위젯으로 묶어주는 Column
         children: [
-          // 상단에 TabBar 배치
           TabBar(
             labelColor: Theme.of(context).primaryColor,
             unselectedLabelColor: Colors.black87,
@@ -24,31 +21,19 @@ class HomeScreen extends StatelessWidget {
                 width: 2.0,
               ),
             ),
-            tabs: [
+            tabs: const [
               Tab(text: "Room-Owner"),
               Tab(text: "Searcher"),
             ],
           ),
-          // 나머지 공간에 TabBarView를 채우기 위해 Expanded 사용
-          Expanded(
+          const Expanded(
             child: TabBarView(
               children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: Sizes.size8,
-                  ),
-                  child: Column(
-                    children: [PostContainer(), PostContainer()],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: Sizes.size8,
-                  ),
-                  child: Column(
-                    children: [PostContainer(), PostContainer()],
-                  ),
-                ),
+                // 각 탭에 PostListView를 배치하고, postType을 전달.
+                PostListView(postType: 'roomOwner'),
+                // TODO: SearcherPost 모델이 준비되면 아래를 활성화
+                // PostListView(postType: 'Searcher'),
+                Center(child: Text("Searcher 게시글 목록 (개발 중)")),
               ],
             ),
           ),
