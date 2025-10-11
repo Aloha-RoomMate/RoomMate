94aafe54bef54a9231a0f1fd36e617eaf9acc336
lee-jaemmin <dlwoals8546@gmail.com>
2025-10-11 20:00:24 +0900

    [REFACTOR] 상수 > 반응형으로 수치 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:roommate/firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:roommate/constants/sizes.dart';
