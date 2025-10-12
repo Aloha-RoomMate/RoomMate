a4f8d5dfe6bb3e4fdfbf018f1ffa42f3308b6707
lee-jaemmin <dlwoals8546@gmail.com>
2025-10-12 17:30:47 +0900

    [REFACTOR] userjob_screen 변경에 따른 구조변경

--- lib/features/authentication/widgets/demand_button.dart
+++ lib/features/authentication/widgets/demand_button.dart
@@ -1,49 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:roommate/constants/responsive_sizes.dart';
 
-class DemandButton extends StatefulWidget {
-  const DemandButton({super.key, required this.text, required this.myonTap});
+class DemandButton extends StatelessWidget {
+  const DemandButton({
+    super.key,
+    required this.text,
+    required this.myonTap,
+    required this.isSelected,
+  });
 
   final String text;
   final Function myonTap;
-
-  @override
-  State<DemandButton> createState() => _DemandButtonState();
-}
-
-class _DemandButtonState extends State<DemandButton> {
-  bool _isSelected = false;
-
-  void _onChipTap() {
-    _isSelected = !_isSelected;
-    widget.myonTap();
-  }
+  final bool isSelected;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => _onChipTap(),
+      onTap: () => myonTap(),
       child: AnimatedContainer(
-        duration: Duration(milliseconds: 300),
+        duration: const Duration(milliseconds: 300),
         padding: EdgeInsets.symmetric(
           vertical: ResponsiveSizes.p(context, 6),
           horizontal: ResponsiveSizes.p(context, 18),
         ),
         decoration: BoxDecoration(
-          color: _isSelected
+          color: isSelected
               ? Theme.of(context).primaryColor
               : Colors.transparent,
           borderRadius: BorderRadius.circular(ResponsiveSizes.p(context, 18)),
           border: Border.all(
-            color: _isSelected ? Colors.transparent : Colors.black38,
+            color: isSelected ? Colors.transparent : Colors.black38,
           ),
         ),
         child: Text(
-          widget.text,
+          text,
           style: TextStyle(
             fontSize: ResponsiveSizes.f(context, 12),
-            color: _isSelected ? Colors.white : Colors.black,
-            fontWeight: _isSelected ? FontWeight.w500 : FontWeight.w400,
+            color: isSelected ? Colors.white : Colors.black,
+            fontWeight: isSelected ? FontWeight.w500 : FontWeight.w400,
           ),
         ),
       ),
