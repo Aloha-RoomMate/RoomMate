abb950f021d7d0bfd0121b10008973c50656d377
ozymandiza421 <skyijin2@gmail.com>
2025-10-26 23:14:14 +0900

    안녕재민아 이게 자동배포야

--- lib/features/authentication/login/login_screen.dart
+++ lib/features/authentication/login/login_screen.dart
@@ -110,7 +110,7 @@ class LoginScreen extends StatelessWidget {
               ),
               Gaps.v20(context),
               Text(
-                '나에게 딱 맞는 룸메이트 찾기',
+                '안녕재민아 이게 자동배포야',
                 style: TextStyle(
                   fontSize: ResponsiveSizes.f(context, 24),
                   fontWeight: FontWeight.w400,
